6c6cc942afa49dab60148cb1b564be408debd88a
RobertaAngioni <robertaangioni92@gmail.com>
2021-11-12 15:18:28 +0100

    primo commit esercizio

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ciao java mi piace molto");
+
+	}
+
+}
